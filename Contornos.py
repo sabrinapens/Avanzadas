d0e6cb920627e78ff16c28227e8aead4f0c261f0
sabrinapens <pensamiento.sabrina@gmail.com>
2025-06-16 21:40:05 -0600

    inicio del programa

---
+++ Contornos.py
@@ -0,0 +1,10 @@
+# Debemos: Calcular el res(fx) trascendentales y racionales
+# Visualizar polos/contornos
+# Estimar inegrales con el TdR
+# Posibles bibliotecas a usar:
+import sympy as sip
+import matplotlib.pyplot as plot
+import numpy as npy
+
+# Variable simbólica, ejemplo:
+z = sip.symbols('z')
